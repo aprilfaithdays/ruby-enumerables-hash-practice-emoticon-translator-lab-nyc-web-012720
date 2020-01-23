333f0a7dbfaee67427f1d9f432021b9d94e7df5a
April Escobar <aprilfaith.e@gmail.com>
2020-01-23 00:22:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ def load_library(file_path)
   result
 end
 
-def get_japanese_emoticon(file_path, emoticon)
+def get_japanese_emoticon(file_path = , emoticon)
   library = load_library(file_path)
   
   if library['get_emoticon'].include?(english_emoticon)
