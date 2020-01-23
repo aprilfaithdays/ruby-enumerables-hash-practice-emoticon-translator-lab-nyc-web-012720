9bdd0fb1fabed49838da23629cb4a3943189aa3f
April Escobar <aprilfaith.e@gmail.com>
2020-01-23 00:17:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,10 @@ def load_library(file_path)
   
   result = {'get_meaning' => {}, 'get_emoticon' => {}}
   
-  library.each do |
+  library.each do |meaning, emoticons|
+    result['get_meaning'][emoticons[1]] = meaning
+    result['get_emoticon'][emoticons[0]] = emoticons[1]
+  end
   
   result
 end
