803c736f60bcd31d668f8d90a2cca8dd3899285b
April Escobar <aprilfaith.e@gmail.com>
2020-01-23 00:15:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,11 @@ require 'yaml'
 
 def load_library(file_path)
   library = YAML.load_file(file_path)
+  
   result = {'get_meaning' => {}, 'get_emoticon' => {}}
+  
+  library.each do |
+  
   result
 end
 
