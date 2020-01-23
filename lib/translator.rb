6b7e16527fa706f852c805cf11e1717557e27a1d
April Escobar <aprilfaith.e@gmail.com>
2020-01-23 00:09:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,8 +2,8 @@ require 'yaml'
 require 'pry'
 
 def load_library(file_path)
-  library = File.read(file_path)
-  emoticons = YAML.loadfile(library)
+  library = YAML.loadfile(file_path)
+  
 end
 
 def get_japanese_emoticon
