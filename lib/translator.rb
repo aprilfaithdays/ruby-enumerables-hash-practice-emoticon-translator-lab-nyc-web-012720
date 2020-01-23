6950f170a54a24e6258d9db1fc64124c076e0910
April Escobar <aprilfaith.e@gmail.com>
2020-01-23 00:24:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,8 +27,8 @@ end
 def get_english_meaning(file_path, japanese_emoticon)
   library = load_library(file_path)
   
-  if library['get_emoticon'].include?(english_emoticon)
-  library['get_emoticon'][english_emoticon]
+  if library['get_meaning'].include?(japanese_emoticon)
+  library['get_meaning'][japanese_emoticon]
   else
     "Sorry, that emoticon was not found"
   end
